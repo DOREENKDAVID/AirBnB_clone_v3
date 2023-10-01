708fff73ba4b519a25dcaf632d9c94881c6b988a
DOREENKDAVID <doreenkinya153@gmail.com>
2023-10-01 13:57:25 -0700

    new view of amenities, places

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-'''Contains the states view for the API.'''
+'''Contains the amenities view for the API.'''
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
-from models.state import State
+from models import Amenity
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def state():
-    """Retrieves the list of all State objects"""
-    objs = storage.all(State)
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def amenity():
+    """Retrieves the list of all amenity objects"""
+    objs = storage.all(Amenity)
     return jsonify([obj.to_dict() for obj in objs.values()]), 200
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def single_state(state_id):
-    """Retrieves a State object"""
-    obj = storage.get(State, state_id)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def single_amenity(amenity_id):
+    """Retrieves a Amenity object"""
+    obj = storage.get(Amenity, amenity_id)
     if not obj:
         abort(404)
     return jsonify(obj.to_dict()), 200
 
 
-@app_views.route('/states/<state_id>',
+@app_views.route('/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
-def del_state(state_id):
-    """Deletes a State object"""
-    obj = storage.get(State, state_id)
+def del_amenity(amenity_id):
+    """Deletes a Amenity object"""
+    obj = storage.get(Amenity, amenity_id)
     if not obj:
         abort(404)
     obj.delete()
@@ -34,24 +34,24 @@ def del_state(state_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def post_state():
-    """Returns the new State with the status code 201"""
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenities():
+    """Returns the new amenities with the status code 201"""
     new_obj = request.get_json()
     if not new_obj:
         abort(400, "Not a JSON")
     if 'name' not in new_obj:
         abort(400, "Missing name")
-    obj = State(**new_obj)
+    obj = Amenity(**new_obj)
     storage.new(obj)
     storage.save()
     return make_response(jsonify(obj.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def put_state(state_id):
-    """ Updates a State object """
-    obj = storage.get(State, state_id)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def put_amenity(amenity_id):
+    """ Updates a amenity object """
+    obj = storage.get(Amenity, amenity_id)
     if not obj:
         abort(404)
 
--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,30 +3,31 @@
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
-from models.state import State
+from models.place import Place
+from models.cities import City
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def state():
-    """Retrieves the list of all State objects"""
-    objs = storage.all(State)
+@app_views.route('/place', methods=['GET'], strict_slashes=False)
+def place():
+    """Retrieves the list of all places objects"""
+    objs = storage.all(Place)
     return jsonify([obj.to_dict() for obj in objs.values()]), 200
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def single_state(state_id):
-    """Retrieves a State object"""
-    obj = storage.get(State, state_id)
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def single_place(place_id):
+    """Retrieves a Places object"""
+    obj = storage.get(Place, place_id)
     if not obj:
         abort(404)
     return jsonify(obj.to_dict()), 200
 
 
-@app_views.route('/states/<state_id>',
+@app_views.route('/places/<place_id>',
                  methods=['DELETE'], strict_slashes=False)
-def del_state(state_id):
-    """Deletes a State object"""
-    obj = storage.get(State, state_id)
+def del_place(place_id):
+    """Deletes a place object"""
+    obj = storage.get(Place, place_id)
     if not obj:
         abort(404)
     obj.delete()
@@ -34,24 +35,25 @@ def del_state(state_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def post_state():
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+def post_places(city_id):
     """Returns the new State with the status code 201"""
     new_obj = request.get_json()
     if not new_obj:
         abort(400, "Not a JSON")
     if 'name' not in new_obj:
         abort(400, "Missing name")
-    obj = State(**new_obj)
+    obj = Place(**new_obj)
     storage.new(obj)
     storage.save()
     return make_response(jsonify(obj.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def put_state(state_id):
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def put_state(place_id):
     """ Updates a State object """
-    obj = storage.get(State, state_id)
+    obj = storage.get(Place, place_id)
     if not obj:
         abort(404)
 
