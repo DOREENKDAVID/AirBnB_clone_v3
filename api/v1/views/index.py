74e49cbb5e452f2d7be19a018bffe322d8ae9d6b
JK-TK <jamexkarix583@gmail.com>
2023-10-01 12:31:56 -0700

    Index.py file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" view of the API"""
-
+'''Contains the index view for the API.'''
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
     """ Returns JSON """
-    return jsonify(status="OK")
+    response = {'status':'OK'}
+    return jsonify(response)
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'])
 def stat():
-    """ number of each objects by type"""
-    return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
+    """returns the number of each objects by type"""
+    status = (
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
     )
+    return jsonify(status)
