3a05af5237d0208018868444034d66b8764092e9
JK-TK <jamexkarix583@gmail.com>
2023-10-01 12:57:17 -0700

    Updating __ini__.py file

---
+++ api/v1/views/cities.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Contains the cities view for the API.'''
+from flask import abort, jsonify, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.state import State
+
+
