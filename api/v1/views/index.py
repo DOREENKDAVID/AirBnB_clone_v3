f46d85242dfb725bada73e398c4f47c37a19d86c
JK-TK <jamexkarix583@gmail.com>
2023-10-01 12:34:38 -0700

    Index file edited

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,22 +5,20 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
-    response = {'status':'OK'}
-    return jsonify(response)
+    return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stat():
     """returns the number of each objects by type"""
-    status = (
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
     )
-    return jsonify(status)
