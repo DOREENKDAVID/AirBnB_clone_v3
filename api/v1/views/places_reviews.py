3b50cb1e55ccaec7f18bc80c081c1f55b45e85c3
JK-TK <jamexkarix583@gmail.com>
2023-10-03 00:52:32 -0700

    Updated

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -2,6 +2,7 @@
 '''Contains the places_reviews view for the API.'''
 from flask import abort, jsonify, make_response, request
 from models import storage
+from api.v1.views import app_views
 from models.place import Place
 from models.review import Review
 from models.user import User
